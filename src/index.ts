99ef26e9d35263798d4cf0257130909e52a8d1ac
ckangwen <2437037469@qq.com>
2022-06-18 14:28:07 +0800

    feat: 移除loadConfigFromFile方法

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,5 @@ export { exec } from "./exec";
 export { findUpMultiple, findUp } from "./find-up";
 export type { FindPathOptions } from "./find-up";
 
-export { loadConfigFromFile } from "./load-config-file";
-
 export { toPath, locatePath } from "./locate-file";
 export type { LocatePathOptions } from "./locate-file";
--- tests/mocks/toolkit.config.ts
+++
@@ -1,9 +0,0 @@
-import { defineConfig, languages } from "./defineConfig";
-
-export default defineConfig({
-  build: {
-    esm: false,
-    ext: languages[0],
-    dependencies: [ "esbuild" ],
-  },
-});
